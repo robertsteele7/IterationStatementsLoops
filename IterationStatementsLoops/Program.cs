e06854308ae483f613105af2a9fb5dd6356136d5
robertsteele7 <atpcycle7@gmail.com>
2022-08-26 11:17:23 -0500

    first commit

---
+++ IterationStatementsLoops/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IterationStatementsLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         /*   for (int i = 0; i < 15; i++) 
+            {
+                Console.WriteLine($"{i}");
+            }
+         */
+        }
+    }
+}
